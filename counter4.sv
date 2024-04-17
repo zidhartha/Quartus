682de0813b78300c49b7d1443016fe9f944f5902
dato <Jincharadze.Davit@kiu.edu.ge>
2024-04-17 23:34:19 +0400

    Update counter4.sv

--- counter4.sv
+++ counter4.sv
@@ -1,37 +1,52 @@
-module counter4 (
-    input clk,input reset,output reg [3:0] count
-);
-always @(posedge clk or negedge reset) begin
-    if (!reset)
-        count <= 4'b0000;
-    else
-        count <= count + 1;
+module counter4(input clk, reset,output reg [3:0] out);  
+ 
+always @ (posedge clk ) begin
+	if(! reset)  
+		out <= 0;
+	else			
+		out <= out + 1;
 end
-endmodule 
-
-module counter_4bit_tb;
-
+ 
+endmodule
+module count_4_bit_test();
+ 
 reg clk;
+
 reg reset;
-wire [3:0] count;
 
-counter4 uut0 (
-    .clk(clk),
-    .reset(reset),
-    .count(count)
+wire [3:0] out;
+ 
+counter4 uut0(
+
+.clk(clk),
+
+.reset(reset),
+
+.out(out)
+
 );
-initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
+ 
+ 
+always #5 clk = ~clk; // change the value of clock every 5 ns
+ 
+initial begin 
+
+clk <= 0;
+
+reset <= 0;
+
+#20;
+
+reset <=1;
+
+#20;
+
+reset <= 0;
+
+#20;
+
+reset <=1;
+
 end
 
-initial begin
-    reset = 1;
-    #10 reset = 0;
-    #50 reset = 1;
-    #10 reset = 0;
-    #100 
-	 $finish;
-end
-
-endmodule 
+endmodule
